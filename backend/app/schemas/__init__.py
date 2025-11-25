f1c54f13a3414bb3c1b3717a4c0ff2bc323a8a50
Alexey <RatnikovSheffer@gmail.com>
2025-11-25 21:28:59 +0300

    correct imports

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -3,3 +3,4 @@ from .user import UserBase  # noqa
 from .week_slots import WeekSlot, WeekSlotsPreferences  # noqa
 from .game import GamePlayerResult, GameOut, GameCreateFromSlot, GameResultsUpdate  # noqa
 from .leaderboard import LeaderboardEntry  # noqa
+from .game_day import *  # noqa
